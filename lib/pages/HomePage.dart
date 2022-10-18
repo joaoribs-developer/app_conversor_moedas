68a1fd9ed58b44a2349f167978adac4e2aeea2fa
João Vitor <joaoribeiroh1@gmail.com>
2022-10-18 20:34:19 -0300

    spinner/moedas

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -11,39 +11,117 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
+  Widget build (BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[800],
         appBar: AppBar(
           backgroundColor: Colors.amber,
           title: Text(
             "Conversor de moedas",
-            textAlign: TextAlign.center,
           ),
         ),
-        body: FutureBuilder<Map>(
-          future: getData(),
-          builder: (context, snapshot){
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return Center(
-                  child: Text(
-                    "Dados Carregando...",
-                    style: TextStyle(
-                      color: Colors.amber,
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+        body: DropDown()
+    );
+  }
+
+}
+class Carregando extends StatelessWidget {
+  const Carregando({Key? key, required this.text}) : super(key: key);
+final String text;
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Text(
+          "$text...",
+          style:const  TextStyle(
+            color: Colors.amber,
+            fontSize: 25,
+          ),
+        ),
+      );
+  }
+}
+
+class DropDown extends StatefulWidget {
+  @override
+  DropDownWidget createState() => DropDownWidget();
+}
+
+class DropDownWidget extends State {
+  String dropdownValue = "USD";
+
+  List <String> spinnerItems = [
+    "USD",
+    "EUR",
+    "GBP",
+    "ARS",
+    "CAD",
+    "AUD",
+    "JPY",
+    "CNY",
+    "BTC"
+  ] ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        height: 400.0,
+        child: SingleChildScrollView(
+          child: Column(
+              children: <Widget>[
+            DropdownButton<String>(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 24,
+              elevation: 16,
+              style: TextStyle(color: Colors.red, fontSize: 18),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? a) {
+                setState(() {
+                   dropdownValue = a!;
+                });
+              },
+              items: spinnerItems.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
                 );
-              default:
-                if(snapshot.hasError){
+              }).toList(),
+            ),
+            Divider(),
+            FutureBuilder<Map>(
+                future: getData(),
+                builder: (context, snapshot){
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return const Carregando(text:"Dados Carregando");
+                    default:
+                      if(snapshot.hasError){
+                        return const Carregando(text: "Erro na obtenção dos dados");
+                      }else{
+                        String data = snapshot.data!["results"]["currencies"]["$dropdownValue"].toString();
+                        return  Container(
+                          height: 200,
+                          child: Text('Selected Item = ' + '$data',
+                              style: TextStyle
+                                (fontSize: 22,
+                                  color: Colors.black)),
+                        );
+                        // Carregando(text: data,);
 
-                }
+                      }
 
-            }
-          },
-        ));
+                  }
+                })
+          ]),
+        ),
+      ),
+    );
   }
 }
