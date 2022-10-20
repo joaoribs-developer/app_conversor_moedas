fed58e82b429ba461efd9945491508495053de1d
João Vitor <joaoribeiroh1@gmail.com>
2022-10-20 00:24:25 -0300

    finalização

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'package:app_conversor_moedas/HttpFactory/httpConfig.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,15 +31,13 @@ class Carregando extends StatelessWidget {
 final String text;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Text(
+    return Text(
           "$text...",
           style:const  TextStyle(
             color: Colors.amber,
             fontSize: 25,
           ),
-        ),
-      );
+        );
   }
 }
 
@@ -64,64 +63,91 @@ class DropDownWidget extends State {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(10.0),
-        height: 400.0,
-        child: SingleChildScrollView(
-          child: Column(
-              children: <Widget>[
-            DropdownButton<String>(
-              value: dropdownValue,
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 24,
-              elevation: 16,
-              style: TextStyle(color: Colors.red, fontSize: 18),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
+    return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+              DropdownButton<String>(
+                value: dropdownValue,
+                icon: Icon(Icons.arrow_drop_down),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.amber, fontSize: 18),
+                underline: Container(
+                  height: 2,
+                  color: Colors.amber,
+                ),
+                onChanged: (String? a) {
+                  setState(() {
+                     dropdownValue = a!;
+                  });
+                },
+                items: spinnerItems.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
               ),
-              onChanged: (String? a) {
-                setState(() {
-                   dropdownValue = a!;
-                });
-              },
-              items: spinnerItems.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            Divider(),
-            FutureBuilder<Map>(
-                future: getData(),
-                builder: (context, snapshot){
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.none:
-                    case ConnectionState.waiting:
-                      return const Carregando(text:"Dados Carregando");
-                    default:
-                      if(snapshot.hasError){
-                        return const Carregando(text: "Erro na obtenção dos dados");
-                      }else{
-                        String data = snapshot.data!["results"]["currencies"]["$dropdownValue"].toString();
-                        return  Container(
-                          height: 200,
-                          child: Text('Selected Item = ' + '$data',
-                              style: TextStyle
-                                (fontSize: 22,
-                                  color: Colors.black)),
-                        );
-                        // Carregando(text: data,);
+                  futureBluider(dropdownValue),
+                  SizedBox()
 
-                      }
-
-                  }
-                })
-          ]),
-        ),
-      ),
-    );
+            ]),
+          ),
+        );
   }
 }
+Widget textView(String value, String text){
+  return Text("$text $value", style: TextStyle(
+    fontSize: 25.0,
+    color: Colors.black
+  ),);
+}
+
+Widget futureBluider([String names = "USD"]){
+  return  FutureBuilder<Map>(
+      future: getData(),
+      builder: (context, snapshot){
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const Carregando(text:"Dados Carregando");
+          default:
+            if(snapshot.hasError){
+              return const Carregando(text: "Erro na obtenção dos dados");
+            }else{
+              String name = snapshot.data!["results"]["currencies"]["$names"]["name"].toString();
+              String buy = snapshot.data!["results"]["currencies"]["$names"]["buy"].toString();
+              String sell = snapshot.data!["results"]["currencies"]["$names"]["sell"].toString();
+              String variation = snapshot.data!["results"]["currencies"]["$names"]["variation"].toString();
+              return Card(
+                color: Colors.blue[200],
+                elevation: 16,
+                child: SizedBox(
+                  height: 250,
+                  width: 400,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        textView(name, "Nome:"),
+                        textView(buy, "Valor de compra R\$:"),
+                        textView(sell, "Valor de vendaR\$:"),
+                        textView(variation, "Variação:")
+                      ],
+                    ),
+                  ),
+                ),
+              );
+              // Carregando(text: data,);
+
+            }
+
+        }
+      });
+}
+
